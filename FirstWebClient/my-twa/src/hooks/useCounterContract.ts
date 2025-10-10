4c479610d527bcb96be5d5834e1f9a2307a1bf55
ToanfKhoa <quachbuitoankhoa@gmail.com>
2025-10-10 14:56:32 +0700

    First web client

---
+++ FirstWebClient/my-twa/src/hooks/useCounterContract.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import Counter from '../contracts/counter';
+import { useTonClient } from './useTonClient';
+import { useAsyncInitialize } from './useAsyncInitialize';
+import { Address } from '@ton/core';
+import type { OpenedContract } from '@ton/core';
+
+export function useCounterContract() {
+  const client = useTonClient();
+  const [val, setVal] = useState<null | number>();
+
+  const counterContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new Counter(
+      Address.parse('kQD0X35KKCgFOv_7p41BUjXC2e_fZz0mFyPZM0WsvyiFYj9X') // replace with your address from tutorial 2 step 8
+    );
+    return client.open(contract) as OpenedContract<Counter>;
+  }, [client]);
+
+  useEffect(() => {
+    async function getValue() {
+      if (!counterContract) return;
+      setVal(null);
+      const val = await counterContract.getCounter();
+      setVal(Number(val));
+    }
+    getValue();
+  }, [counterContract]);
+
+  return {
+    value: val,
+    address: counterContract?.address.toString(),
+  };
+}
